ab1325b1eec310ecd8ae140af05ecea9451b9e3b
Sil <31925535+silvappeldoorn@users.noreply.github.com>
2019-06-13 20:17:59 +0200

    code afvink 5

--- app.py
+++ app.py
@@ -9,8 +9,39 @@ pizzaDB = [
 ]
 
 
-@app.route("/", methods=['GET'])
-def getPizza():
+@app.route("/<string:name>", methods=['GET'])
+def getOnePizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    return jsonify({'pizzaDB': resultPizza})
+
+
+@app.route("/", methods=['POST'])
+def addOnePizza():
+    pizza = {'name': request.json['name']}
+    pizzaDB.append(pizza)
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+@app.route("/<string:name>", methods=['PUT'])
+def putPizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    resultPizza[0]['name'] = request.json['name']
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+@app.route("/<string:name>", methods=['DELETE'])
+def delPizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    pizzaDB.remove(resultPizza[0])
     return jsonify({'pizzaDB': pizzaDB})
 
 
