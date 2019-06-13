f739335f642e52f11916cdd0fdb21e1132e6fe90
Sil <31925535+silvappeldoorn@users.noreply.github.com>
2019-06-13 20:08:01 +0200

    opdracht 1 afvinkopdracht 5

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+pizzaDB = [
+    {'name': 'tonno'},
+    {'name': 'salami'},
+    {'name': 'hawaii'}
+]
+
+
+@app.route("/", methods=['GET'])
+def getPizza():
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
